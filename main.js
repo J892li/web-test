addd83afd66c99049bf33a4335f38e1405cc9762
Lily Li <51288118+J892li@users.noreply.github.com>
2019-11-24 10:29:26 +0800

    Add files via upload

--- main.js
+++ main.js
@@ -93,3 +93,13 @@ function navFunction() {
     x.className = "nav";
   }
 }
+
+// When the user scrolls the page, execute myFunction
+window.onscroll = function() {myFunction()};
+
+// Get the navbar
+var navbar = document.getElementById("navbar");
+
+// Get the offset position of the navbar
+var sticky = navbar.offsetTop;
+
